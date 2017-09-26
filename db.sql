833265e46a863e8988c98e64d2037b84c4af71c7
Iwo <iwo.tyszkowski@gmail.com>
2017-09-26 03:41:54 +0200

    a

--- db.sql
+++ db.sql
@@ -1,16 +1,16 @@
 CREATE TABLE mentors (
-  id INTEGER,
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
   first_name TEXT NOT NULL,
   last_name TEXT NOT NULL,
   nick_name TEXT NOT NULL,
   phone_number TEXT NOT NULL UNIQUE,
   email TEXT NOT NULL UNIQUE,
   city TEXT NOT NULL,
-  favourite_number INTEGER,
-  PRIMARY KEY(id)
+  favourite_number INTEGER
 );
+
 CREATE TABLE applicants (
-  id INTEGER PRIMARY KEY,
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
   first_name TEXT NOT NULL,
   last_name TEXT NOT NULL,
   phone_number TEXT NOT NULL UNIQUE,
