f9e82d208a2348e75da0d340b92dfc4aee839e5f
Iwo <iwo.tyszkowski@gmail.com>
2017-09-26 02:22:49 +0200

    a

--- db.sql
+++ db.sql
@@ -1,4 +1,19 @@
 CREATE TABLE mentors (
+  id INTEGER,
+  first_name TEXT NOT NULL,
+  last_name TEXT NOT NULL,
+  nick_name TEXT NOT NULL,
+  phone_number TEXT NOT NULL UNIQUE,
+  email TEXT NOT NULL UNIQUE,
+  city TEXT NOT NULL,
+  favourite_number INTEGER,
+  PRIMARY KEY(id)
+);
+CREATE TABLE applicants (
   id INTEGER PRIMARY KEY,
-
+  first_name TEXT NOT NULL,
+  last_name TEXT NOT NULL,
+  phone_number TEXT NOT NULL UNIQUE,
+  email TEXT UNIQUE,
+  application_code INTEGER UNIQUE
 );
